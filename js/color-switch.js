eeeb123b8549076b7022184c46f3c908a6bbd41d
Yuliia Danilenkova <82413695+danilenkova@users.noreply.github.com>
2021-11-04 22:18:21 +0200

    Add colorSwitch

---
+++ js/color-switch.js
@@ -0,0 +1,40 @@
+const refs = {
+  colors: [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ],
+  body: document.querySelector('body'),
+  startButton: document.querySelector('[data-action="start"]'),
+  stopButton: document.querySelector('[data-action="stop"]'),
+  randomIntegerFromInterval: (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  changeColor() {
+    do {
+      newColor = this.colors[this.randomIntegerFromInterval(0, this.colors.length - 1)];
+      this.body.style.background = newColor;
+    } while (newColor === currentColor) {
+      this.body.style.background = newColor;
+      currentColor = newColor;
+    }
+  },
+};
+
+let currentColor, newColor = '#FFFFFF';
+let timerId = null;
+
+refs.startButton.addEventListener("click", () => {
+  refs.startButton.setAttribute("disabled", true);
+  timerId = setInterval(() => {
+  refs.changeColor();
+  }, 1000);
+});
+
+refs.stopButton.addEventListener("click", () => {
+  clearInterval(timerId);
+  refs.startButton.removeAttribute("disabled");
+});
